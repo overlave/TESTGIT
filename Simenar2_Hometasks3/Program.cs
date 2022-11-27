ff2f3cb01b3aed1f88b4bd4f1603b45b69a65d51
overlave <dom142da@bk.ru>
2022-11-27 21:00:55 +0300

    done hometasks

---
+++ Simenar2_Hometasks3/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear ();
+
+Console.WriteLine("Введите день недели: ");
+int data = Convert.ToInt32(Console.ReadLine());
+
+if (data > 7)
+Console.WriteLine("День недели не может быть больше 7");
+else if (data > 5)
+Console.WriteLine("Выходной");
+else if (data > 0)
+Console.WriteLine("Будний день");
+else
+Console.WriteLine("День недели не может быть меньше 1");
+
+// если день болльше семи то  Console.WriteLine("День недели не может быть больше 7"); 
+
+// если меьше семи то проверяем следующие условие больше 5 да перепроверяем первое if меньше 7 Console.WriteLine("Выходной");
+
+//если меньше семи то проверяем следующие условие больше 5 нет больше 0 да Console.WriteLine("Будний день");
+
+// //если меньше семи то проверяем следующие условие больше 5 нет больше 0 нет Console.WriteLine("День недели не может быть меньше 1");
+
+
