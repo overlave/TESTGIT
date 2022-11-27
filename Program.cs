024842249b1fffab71368b64730e6729a03b20a5
overlave <118617074+overlave@users.noreply.github.com>
2022-11-27 21:17:26 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear ();
+
+Console.WriteLine("Введите трехзначное число: ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+int n1 = n / 100;
+int n2 = (n - (n1 * 100))/10;
+
+if (n < 100 || n > 999)
+Console.WriteLine("Число не трехзначное");
+
+else
+Console.WriteLine(n2);
